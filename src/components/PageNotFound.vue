978e0d602a9211d536987ba3efff87546d28b964
yethuraza <151596674+yethuraza@users.noreply.github.com>
2024-02-08 12:29:37 +0630

    not found

---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="col-full text-center">
+    <h1>Page Not Found</h1>
+    <router-link :to="{ name: 'Home' }"
+      >Read some awesome Threads!!</router-link
+    >
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,33 @@
 import { createRouter, createWebHistory } from "vue-router";
-
+import sourceData from "@/data.json";
 import PageHomeVue from "../components/PageHome.vue";
 import ThreadShowPage from "../components/ThreadShowPage.vue";
+import PageNotFound from "../components/PageNotFound.vue";
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", name: "Home", component: PageHomeVue },
-    { path: "/thread/:id", name: "ThreadShow", component: ThreadShowPage },
+    {
+      path: "/thread/:id",
+      name: "ThreadShow",
+      component: ThreadShowPage,
+      beforeEnter(to, from, next) {
+        const threadExist = sourceData.threads.find(
+          (t) => t.id === to.params.id
+        );
+        if (threadExist) {
+          return next();
+        } else {
+          next({
+            name: "NotFound",
+            params: { pathMatch: to.path.substring(1).split("/") },
+            query: to.query,
+            hash: to.hash,
+          });
+        }
+      },
+    },
+    { path: "/:pathMatch(.*)*", name: "NotFound", component: PageNotFound },
   ],
 });
 
