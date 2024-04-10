469cf9979a38f6f2454b909f0a692fb2db84020e
yethuraza <ytr@za.com.mm>
2024-04-10 15:52:48 +0630

    button , dialog

--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@ import "./style.css";
 import App from "./App.vue";
 import router from "@/router";
 import PrimeVue from "primevue/config";
+import "primeflex/primeflex.css";
 import "primevue/resources/themes/aura-light-green/theme.css";
+import "primevue/resources/primevue.min.css"; /* Deprecated */
+import "primeicons/primeicons.css";
 import Button from "primevue/button";
+import InputText from "primevue/inputtext";
 
 const app = createApp(App);
 app.component("Button", Button);
+app.component("InputText", InputText);
 
-app.use(PrimeVue).use(router).mount("#app");
+app.use(PrimeVue, { ripple: true }).use(router).mount("#app");
